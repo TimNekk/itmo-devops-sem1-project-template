a19238ea53ad41930958d338150ad948d8de5f20
Tim <herew26@gmail.com>
2025-12-05 14:57:22 +0300

    🔥 Remove fatalf

--- main.go
+++ main.go
@@ -2,23 +2,29 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	if err := run(); err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
 	connectDB()
 	defer closeDB()
 
 	if err := initDB(); err != nil {
-		log.Fatalf("Failed to initialize database: %v", err)
+		return err
 	}
 
 	r := gin.Default()
 	r.POST("/api/v0/prices", uploadPrices)
 	r.GET("/api/v0/prices", getPrices)
 
-	if err := r.Run(); err != nil {
-		log.Fatalf("r.Run() failed: %v", err)
-	}
+	return r.Run()
 }
